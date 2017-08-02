04ee811d315324db09346e0e6b4c9e90e344658b
Anna Fuentes Jakobsen <annafjakobsen@gmail.com>
2017-08-02 11:29:47 +0200

    Added Sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './styles.css'
+import './style/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
